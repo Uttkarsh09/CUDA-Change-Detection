bad24b575c2367f82fef14fda19b2d28cab283f7
Uttkarsh Patel <uttkarshpatel9@gmail.com>
2023-03-20 02:44:33 +0530

    updated machine information print stmt

--- src/machineInformation.cu
+++ src/machineInformation.cu
@@ -8,11 +8,32 @@ void printMachineInformation(cudaDeviceProp prop){
 		cudaGetDeviceProperties(&prop, i);
 	}
 
-	printf("~~~ CUDA INFORMATION ~~~\n");
-	printf("Number of GPUs - %d\n", count);
-	printf("Name of GPU - %s\n", prop.name);
-	printf("Major Compute Capability - %d\n", prop.major);
-	printf("Minor Compute Capability - %d\n", prop.minor);
-	printf("Total Global Memory - %zu\n", prop.totalGlobalMem);
-	printf("Shared Memory per Block - %zu\n", prop.sharedMemPerBlock);
+	printf("\n~~~~~~~~~~ General Information ~~~~~~~~~~\n");
+	printf("Name:	%s\n", prop.name );
+	printf("Compute capability: %d.%d\n", prop.major, prop.minor );
+	printf("Clock rate: %d\n", prop.clockRate );
+	printf("Device copy overlap:	");
+	if (prop.deviceOverlap)
+		printf("Enabled\n");
+	else
+		printf("Disabled\n");
+	printf("Kernel execition timeout :	");
+	if (prop.kernelExecTimeoutEnabled)
+		printf("Enabled\n");
+	else
+		printf("Disabled\n");
+	printf("\n~~~~~~~~~~ Memory Information ~~~~~~~~~~\n");
+	printf("Total global mem:	%ld\n", prop.totalGlobalMem);
+	printf("Total constant Mem:%ld\n", prop.totalConstMem);
+	printf("Max mem pitch:	%ld\n", prop.memPitch);
+	printf("Texture Alignment:	%ld\n", prop.textureAlignment);
+	printf("\n~~~~~~~~~~ MP Information ~~~~~~~~~~\n");
+	printf("Multiprocessor count:	%d\n", prop.multiProcessorCount);
+	printf("Shared mem per mp:	%ld\n", prop.sharedMemPerBlock);
+	printf("Registers per mp:	%d\n", prop.regsPerBlock);
+	printf("Threads in warp:	%d\n", prop.warpSize);
+	printf("Max threads per block:	%d\n", prop.maxThreadsPerBlock );
+	printf("Max thread dimensions:	(%d, %d, %d)\n", prop.maxThreadsDim[0], prop.maxThreadsDim[1],	prop.maxThreadsDim[2]);
+	printf("Max grid dimensions:	(%d, %d, %d)\n",	prop.maxGridSize[0], prop.maxGridSize[1],	prop.maxGridSize[2]);
+	printf("\n");
 }
--- src/main.cu
+++ src/main.cu
@@ -7,8 +7,8 @@ __global__ void add(int a, int b, int *c){
 
 int main(){
 	int deviceCount=-1;
-	// int c;
-	// int *dev_c;
+	int c;
+	int *dev_c;
 	cudaDeviceProp deviceProperties;
 
 	cudaGetDeviceCount(&deviceCount);
@@ -19,10 +19,10 @@ int main(){
 	}
 
 
-	// cudaMalloc((void**)&dev_c, sizeof(int));
-	// add<<<1,1>>>(2, 7, dev_c);
-	// cudaMemcpy(&c, dev_c, sizeof(int), cudaMemcpyDeviceToHost);
-	// printf("answer = %d", c);
-	// cudaFree(dev_c);
+	cudaMalloc((void**)&dev_c, sizeof(int));
+	add<<<1,1>>>(2, 7, dev_c);
+	cudaMemcpy(&c, dev_c, sizeof(int), cudaMemcpyDeviceToHost);
+	printf("answer = %d", c);
+	cudaFree(dev_c);
 	return 0;
 }

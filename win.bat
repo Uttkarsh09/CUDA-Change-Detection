b3198d6c6d557add7a2f4f433aeded436bb09bc2
AtharvNatu <natuatharv21@gmail.com>
2023-04-16 01:30:42 +0530

    OpenCL-CUDA Build Script

--- win.bat
+++ win.bat
@@ -10,13 +10,13 @@ if %hpp%==CUDA (
     
     nvcc.exe -c -o cudaProperties.obj src/GPU/CUDA/cudaProperties.cu
     cl.exe /c /EHsc /std:c++20 /I "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.0\include" src/main.cpp src/common/imageFunctions.cpp src/CPU/changeDetection.cpp src/CPU/imageOperations.cpp
-    link.exe /LIBPATH:"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.0\lib\x64" /LIBPATH:"./lib" Main.obj cudaProperties.obj imageFunctions.obj changeDetection.obj imageOperations.obj
-    Main.exe
+    link.exe /LIBPATH:"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.0\lib\x64" /LIBPATH:"./lib" main.obj cudaProperties.obj imageFunctions.obj changeDetection.obj imageOperations.obj
+    main.exe
 
 ) else (
 
-    cl.exe /c /EHsc /std:c++20 /I "C:\OCL_SDK_Light\include" /I ".\include" src/Main.cpp src/OpenCLProperties.cpp src/ImageFunctions.cpp
-    link.exe /LIBPATH:"C:\OCL_SDK_Light\lib\x86_64" Main.obj OpenCLProperties.obj ImageFunctions.obj
-    Main.exe
+    cl.exe /c /EHsc /std:c++20 /I "C:\OCL_SDK_Light\include" /I ".\include" src/main.cpp src/GPU/OpenCL/openclProperties.cpp src/common/imageFunctions.cpp src/CPU/changeDetection.cpp src/CPU/imageOperations.cpp
+    link.exe /LIBPATH:"C:\OCL_SDK_Light\lib\x86_64" /LIBPATH:"./lib" main.obj openclProperties.obj imageFunctions.obj changeDetection.obj imageOperations.obj
+    main.exe
 
 )

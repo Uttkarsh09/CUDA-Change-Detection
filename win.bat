157237d27f938cb3fb451d6bed5a988a3d073c93
AtharvNatu <natuatharv21@gmail.com>
2023-04-16 02:38:51 +0530

    Bin Folder

--- win.bat
+++ win.bat
@@ -8,15 +8,21 @@ if ERRORLEVEL 1 (set hpp=OpenCL) else (set hpp=CUDA)
 
 if %hpp%==CUDA (
     
-    nvcc.exe -c -o cudaProperties.obj src/GPU/CUDA/cudaProperties.cu
-    cl.exe /c /EHsc /std:c++20 /I "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.0\include" src/main.cpp src/common/imageFunctions.cpp src/CPU/changeDetection.cpp src/CPU/imageOperations.cpp
-    link.exe /LIBPATH:"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.0\lib\x64" /LIBPATH:"./lib" main.obj cudaProperties.obj imageFunctions.obj changeDetection.obj imageOperations.obj
+    nvcc.exe -c -o "./bin/cudaProperties.obj" src/GPU/CUDA/cudaProperties.cu
+    cd "./bin/"
+    cl.exe /c /EHsc /std:c++20 /I "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.0\include" "../src/main.cpp" "../src/common/imageFunctions.cpp" "../src/CPU/changeDetection.cpp" "../src/CPU/imageOperations.cpp"
+    link.exe /LIBPATH:"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.0\lib\x64" /LIBPATH:"../lib" main.obj imageFunctions.obj changeDetection.obj imageOperations.obj cudaProperties.obj
+    @copy main.exe "../" > nul
+    cd ../
     main.exe
 
 ) else (
 
-    cl.exe /c /EHsc /std:c++20 /I "C:\OCL_SDK_Light\include" /I ".\include" src/main.cpp src/GPU/OpenCL/openclProperties.cpp src/common/imageFunctions.cpp src/CPU/changeDetection.cpp src/CPU/imageOperations.cpp
-    link.exe /LIBPATH:"C:\OCL_SDK_Light\lib\x86_64" /LIBPATH:"./lib" main.obj openclProperties.obj imageFunctions.obj changeDetection.obj imageOperations.obj
+    cd "./bin/"
+    cl.exe /c /EHsc /std:c++20 /I "C:\OCL_SDK_Light\include" "../src/main.cpp" "../src/GPU/OpenCL/openclProperties.cpp" "../src/common/imageFunctions.cpp" "../src/CPU/changeDetection.cpp" "../src/CPU/imageOperations.cpp"
+    link.exe /LIBPATH:"C:\OCL_SDK_Light\lib\x86_64" /LIBPATH:"../lib" main.obj openclProperties.obj imageFunctions.obj changeDetection.obj imageOperations.obj
+    @copy main.exe "../" > nul
+    cd ../
     main.exe
 
 )

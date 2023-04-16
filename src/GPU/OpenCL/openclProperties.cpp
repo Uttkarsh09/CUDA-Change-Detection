f63c551ef866bad605b039b3bad2cd04b097fda5
AtharvNatu <natuatharv21@gmail.com>
2023-04-16 14:50:51 +0530

    OpenCL Version and Image Support

--- src/GPU/OpenCL/openclProperties.cpp
+++ src/GPU/OpenCL/openclProperties.cpp
@@ -12,6 +12,7 @@ void printOpenCLDeviceProperties(void)
 	cl_platform_id ocl_platform_id;
 	cl_uint dev_count, compute_units;
 	cl_ulong mem_size;
+	cl_bool image_support;
 	cl_device_id* ocl_device_ids;
 	char oclPlatformInfo[512], ocl_dev_prop[1024];
 
@@ -59,12 +60,21 @@ void printOpenCLDeviceProperties(void)
 			
 			clGetDeviceInfo(ocl_device_ids[i], CL_DEVICE_VENDOR, sizeof(ocl_dev_prop), &ocl_dev_prop, NULL);
 			cout << endl << "GPU Device Vendor				: " << ocl_dev_prop;
+
+			clGetDeviceInfo(ocl_device_ids[i], CL_DEVICE_VERSION, sizeof(ocl_dev_prop), &ocl_dev_prop, NULL);
+			cout << endl << "GPU Device OpenCL Version			: " << ocl_dev_prop;
 			
 			clGetDeviceInfo(ocl_device_ids[i], CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(mem_size), &mem_size, NULL);
 			cout << endl << "GPU Device Memory				: " << (unsigned long long) mem_size / 1000000000 << " GB";
 			
 			clGetDeviceInfo(ocl_device_ids[i], CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(compute_units), &compute_units, NULL);
 			cout << endl << "GPU Device Number Of Parallel Processor Cores	: " << compute_units;
+			
+			clGetDeviceInfo(ocl_device_ids[i], CL_DEVICE_IMAGE_SUPPORT, sizeof(image_support), &image_support, NULL);
+			if (image_support == CL_TRUE)
+				cout << endl << "GPU Device Image Support			: Yes";
+			else
+				cout << endl << "GPU Device Image Support			: No";
 		}
 
 		free(ocl_device_ids);
--- win.bat
+++ win.bat
@@ -8,10 +8,17 @@ if ERRORLEVEL 1 (set hpp=OpenCL) else (set hpp=CUDA)
 
 if %hpp%==CUDA (
     
-    nvcc.exe -c -o "./bin/cudaProperties.obj" src/GPU/CUDA/cudaProperties.cu
+    @REM nvcc.exe -c -o "./bin/cudaProperties.obj" src/GPU/CUDA/cudaProperties.cu
+    @REM cd "./bin/"
+    @REM cl.exe /c /EHsc /std:c++20 /I "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.0\include" "../src/main.cpp" "../src/common/imageFunctions.cpp" "../src/CPU/changeDetection.cpp" "../src/CPU/imageOperations.cpp"
+    @REM link.exe /LIBPATH:"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.0\lib\x64" /LIBPATH:"../lib" main.obj imageFunctions.obj changeDetection.obj imageOperations.obj cudaProperties.obj
+    @REM @copy main.exe "../" > nul
+    @REM cd ../
+    @REM main.exe
+
     cd "./bin/"
-    cl.exe /c /EHsc /std:c++20 /I "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.0\include" "../src/main.cpp" "../src/common/imageFunctions.cpp" "../src/CPU/changeDetection.cpp" "../src/CPU/imageOperations.cpp"
-    link.exe /LIBPATH:"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.0\lib\x64" /LIBPATH:"../lib" main.obj imageFunctions.obj changeDetection.obj imageOperations.obj cudaProperties.obj
+    cl.exe /c /EHsc /std:c++20 /I "C:\OCL_SDK_Light\include" "../src/main.cpp" "../src/GPU/OpenCL/openclProperties.cpp" "../src/common/imageFunctions.cpp" "../src/CPU/changeDetection.cpp" "../src/CPU/imageOperations.cpp"
+    link.exe /LIBPATH:"C:\OCL_SDK_Light\lib\x86_64" /LIBPATH:"../lib" main.obj openclProperties.obj imageFunctions.obj changeDetection.obj imageOperations.obj
     @copy main.exe "../" > nul
     cd ../
     main.exe

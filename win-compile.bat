5a766f563eac12094ed7cb832eae22dbe21d6d2b
Atharv Natu <natuatharv21@gmail.com>
2023-05-19 19:51:25 +0530

    Build Windows

---
+++ win-compile.bat
@@ -0,0 +1,37 @@
+
+cls
+
+@REM GPU Detection
+@echo off
+where /q nvcc
+if ERRORLEVEL 1 (set hpp=OpenCL) else (set hpp=CUDA)
+
+if %hpp%==CUDA (
+    
+    nvcc.exe -std c++20 -c -o "./bin/cudaChangeDetection.obj" src/GPU/CUDA/cudaChangeDetection.cu
+    nvcc.exe -std c++20 -c -o "./bin/cudaChangeDetectionKernel.obj" src/GPU/CUDA/changeDetectionKernel.cu
+    cd "./bin/"
+    cl.exe /c /EHsc /std:c++20 /I "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.0\include" "../src/main.cpp" "../src/common/imageFunctions.cpp" "../src/CPU/changeDetection.cpp" "../src/CPU/imageOperations.cpp"
+    link.exe /LIBPATH:"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.0\lib\x64" /LIBPATH:"../lib" main.obj imageFunctions.obj changeDetection.obj imageOperations.obj cudaChangeDetection.obj cudaChangeDetectionKernel.obj
+    @copy main.exe "../" > nul
+    cd ../
+    main.exe 1024 1
+    main.exe 2048 0
+    main.exe 4096 0
+    main.exe 8192 0
+    main.exe 10000 0
+
+) else (
+
+    cd "./bin/"
+    cl.exe /c /EHsc /std:c++20 /I "C:\OCL_SDK_Light\include" "../src/main.cpp" "../src/GPU/OpenCL/openclChangeDetection.cpp" "../src/common/imageFunctions.cpp" "../src/CPU/changeDetection.cpp" "../src/CPU/imageOperations.cpp"
+    link.exe /LIBPATH:"C:\OCL_SDK_Light\lib\x86_64" /LIBPATH:"../lib" main.obj openclChangeDetection.obj imageFunctions.obj changeDetection.obj imageOperations.obj
+    @copy main.exe "../" > nul
+    cd ../
+    main.exe 1024 1
+    main.exe 2048 0
+    main.exe 4096 0
+    main.exe 8192 0
+    main.exe 10000 0
+
+)

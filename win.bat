c4592546255a0126c0e06e83fcf9071ff555fb39
AtharvNatu <natuatharv21@gmail.com>
2023-05-19 18:31:06 +0530

    OpenCL-CUDA Build

--- win.bat
+++ win.bat
@@ -8,22 +8,11 @@ if ERRORLEVEL 1 (set hpp=OpenCL) else (set hpp=CUDA)
 
 if %hpp%==CUDA (
     
-    @REM nvcc.exe -std c++20 -c -o "./bin/cudaChangeDetection.obj" src/GPU/CUDA/cudaChangeDetection.cu
-    @REM nvcc.exe -std c++20 -c -o "./bin/cudaChangeDetectionKernel.obj" src/GPU/CUDA/changeDetectionKernel.cu
-    @REM cd "./bin/"
-    @REM cl.exe /c /EHsc /std:c++20 /I "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.0\include" "../src/main.cpp" "../src/common/imageFunctions.cpp" "../src/CPU/changeDetection.cpp" "../src/CPU/imageOperations.cpp"
-    @REM link.exe /LIBPATH:"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.0\lib\x64" /LIBPATH:"../lib" main.obj imageFunctions.obj changeDetection.obj imageOperations.obj cudaChangeDetection.obj cudaChangeDetectionKernel.obj
-    @REM @copy main.exe "../" > nul
-    @REM cd ../
-    @REM main.exe 1024 1
-    @REM main.exe 2048 0
-    @REM main.exe 4096 0
-    @REM main.exe 8192 0
-    @REM main.exe 10000 0
-
+    nvcc.exe -std c++20 -c -o "./bin/cudaChangeDetection.obj" src/GPU/CUDA/cudaChangeDetection.cu
+    nvcc.exe -std c++20 -c -o "./bin/cudaChangeDetectionKernel.obj" src/GPU/CUDA/changeDetectionKernel.cu
     cd "./bin/"
-    cl.exe /c /EHsc /std:c++20 /I "C:\OCL_SDK_Light\include" "../src/main.cpp" "../src/GPU/OpenCL/openclChangeDetection.cpp" "../src/common/imageFunctions.cpp" "../src/CPU/changeDetection.cpp" "../src/CPU/imageOperations.cpp"
-    link.exe /LIBPATH:"C:\OCL_SDK_Light\lib\x86_64" /LIBPATH:"../lib" main.obj openclChangeDetection.obj imageFunctions.obj changeDetection.obj imageOperations.obj
+    cl.exe /c /EHsc /std:c++20 /I "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.0\include" "../src/main.cpp" "../src/common/imageFunctions.cpp" "../src/CPU/changeDetection.cpp" "../src/CPU/imageOperations.cpp"
+    link.exe /LIBPATH:"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.0\lib\x64" /LIBPATH:"../lib" main.obj imageFunctions.obj changeDetection.obj imageOperations.obj cudaChangeDetection.obj cudaChangeDetectionKernel.obj
     @copy main.exe "../" > nul
     cd ../
     main.exe 1024 1
@@ -39,6 +28,10 @@ if %hpp%==CUDA (
     link.exe /LIBPATH:"C:\OCL_SDK_Light\lib\x86_64" /LIBPATH:"../lib" main.obj openclChangeDetection.obj imageFunctions.obj changeDetection.obj imageOperations.obj
     @copy main.exe "../" > nul
     cd ../
-    main.exe
+    main.exe 1024 1
+    main.exe 2048 0
+    main.exe 4096 0
+    main.exe 8192 0
+    main.exe 10000 0
 
 )
